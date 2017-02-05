b2ac71051dd6e27b4747a32b4ebfbb7bff918235
som <alleyful@naver.com>
2017-02-05 12:47:58 +0900

    github test

---
+++ src/app.js
@@ -0,0 +1,8 @@
+var math = require('./math');
+var $ = require('jquery');
+
+function a(){
+    return 1111;
+}
+
+console.log($('body'));
---
+++ src/math.js
@@ -0,0 +1,7 @@
+var math = {
+    max: function(){},
+    min: function(){}
+};
+console.log(math);
+
+module.exports = math;
