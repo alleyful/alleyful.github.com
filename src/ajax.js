9cf374704587646ddb194e88a2b39e07b5d75df4
som <alleyful@naver.com>
2017-02-12 12:48:44 +0900

    스터디 170212

---
+++ src/ajax.js
@@ -0,0 +1,5 @@
+//module.exports = function(){};
+
+export default function(url, callback){
+
+};
--- src/app.js
+++ src/app.js
@@ -1,8 +1,34 @@
-var math = require('./math');
-var $ = require('jquery');
+//const tplList = require('./list.hbs');
+import tplList from './list.hbs';
+//const tplMain = require('./main.hbs');
+import tplMain from './main.hbs';
+import ajax from './ajax.js';
 
-function a(){
-    return 1111;
-}
+ajax('/data.json', function(data){
+    return data+1;
+});
+ajax('/data.json', data => data+1);
 
-console.log($('body'));
+
+
+[].map(v = v+1);
+[{price:300}].filter(v => {
+    return v.price > 200
+})
+
+const $ = require('jquery');
+
+$('#root').html(tplMain);
+
+
+const list = [{
+    href: 'http://www.naver.com',
+    name: 'naver'
+},{
+    href:'http://www.daum.net',
+    name:'daum'
+}];
+
+$('[data-view="list"]').html(tplList({
+    list: list
+}));
