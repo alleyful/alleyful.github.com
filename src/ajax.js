c19302aac5a850895d85b807149f659205b018fd
som <alleyful@naver.com>
2017-02-14 19:59:56 +0900

    170212 과제1

--- src/ajax.js
+++ src/ajax.js
@@ -1,5 +1,13 @@
 //module.exports = function(){};
+export default (url, callback) => {
+    const xhr = new XMLHttpRequest();
 
-export default function(url, callback){
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4 && xhr.status === 200){
+            callback(JSON.parse(xhr.responseText));
+        }
+    };
 
+    xhr.open('get', url, true);
+    xhr.send();
 };
--- src/app.js
+++ src/app.js
@@ -1,25 +1,50 @@
-//const tplList = require('./list.hbs');
 import tplList from './list.hbs';
-//const tplMain = require('./main.hbs');
 import tplMain from './main.hbs';
+import tplTable from './table.hbs';
+import tplWeather from './weather.hbs';
 import ajax from './ajax.js';
-
-ajax('/data.json', function(data){
-    return data+1;
-});
-ajax('/data.json', data => data+1);
-
-
-//
-// [].map(v = v+1);
-// [{price:300}].filter(v => {
-//     return v.price > 200
-// })
-
 const $ = require('jquery');
 
 $('#root').html(tplMain);
 
+const createTable  = () => {
+    ajax('https://raw.githubusercontent.com/suhokim2/suhokim2.github.com/master/data.json', data => {
+        const tableList = data.fruits.map(fruit => {
+            return {
+                name: fruit.name,
+                quantity:fruit.quantity,
+                price:fruit.price
+            }
+        });
+        const tableTotal = fruitSum(tableList);
+        $('#tableBox').html(tplTable({
+             tableList:tableList,
+             tableTotal:tableTotal
+        }));
+    });
+}
+
+const createWeather  = () => {
+    ajax('http://api.openweathermap.org/data/2.5/forecast/daily?q=seoul&mode=json&units=metric&cnt=7&apikey=8d554a626fc5d01d77812b612a6de257', data => {
+        // const weatherList = data.list.map(weather => {
+        //     return {
+        //
+        //     }
+        // })
+        console.log(data);
+        // $('#tableBox').html(tplWeather({
+        //     weatherList:weatherList
+        // }));
+    });
+}
+
+const fruitSum = (arr) => {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i].quantity * arr[i].price;
+    }
+    return result;
+};
 
 const list = [{
     href: 'http://www.naver.com',
@@ -29,6 +54,30 @@ const list = [{
     name:'daum'
 }];
 
+
 $('[data-view="list"]').html(tplList({
     list: list
 }));
+
+
+$('#btnTable').on('click',(e) => {
+    if($('#btnTable').hasClass('mdl-button--accent')){
+        $('#tableBox').html('');
+        $('#btnTable').removeClass('mdl-button--accent');
+    } else {
+        $('#btnTable').addClass('mdl-button--accent');
+        createTable();
+    }
+});
+
+$('#btnWeather').on('click',(e) => {
+    if($('#btnWeather').hasClass('mdl-button--accent')){
+        $('#tableBox').html('');
+        $('#btnWeather').removeClass('mdl-button--accent');
+    } else {
+        $('#btnWeather').addClass('mdl-button--accent');
+        $('#tableBox').html('');
+        createWeather();
+    }
+});
+
